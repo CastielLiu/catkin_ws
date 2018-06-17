6018e6bf6b837a4fd23f8ddb0c47a874350235a8
CastielLiu <castiel_liu@outlook.com>
2018-06-17 10:19:04 +0800

    add a tf test

---
+++ src/driverless/src/tf/tf.cpp
@@ -0,0 +1,22 @@
+#include<ros/ros.h>
+#include<tf/transform_broadcaster.h>
+
+int main(int argc ,char** argv)
+{
+	ros::init(argc,argv,"car_tf_publisher");
+	ros::NodeHandle n;
+	
+	ros::Rate r(100);
+	
+	tf::TransformBroadcaster broadcaster;
+	
+	while(ros::ok())
+	{
+		broadcaster.sendTransform(
+			tf::StampedTransform(tf::Transform(tf::Quaternion(0,0,0,0),tf::Vector3(0.1,0.0,0.2)),
+								  ros::Time::now(),
+								  "base_link",
+								  "lidar_frame"));
+		r.sleep();
+	}
+}
