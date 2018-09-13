3847245ee4bd2bf47b535b5cc7a85b539fa239f2
CastielLiu <castiel_liu@outlook.com>
2018-09-13 23:10:39 +0800

    add use key to record point

--- src/driverless/src/record_data/record_data.cpp
+++ src/driverless/src/record_data/record_data.cpp
@@ -1,29 +1,39 @@
 #include<ros/ros.h>
 #include<driverless/Gps.h>
+#include <unistd.h>
+#include <linux/input.h>
+#include <fcntl.h>
+#include <std_msgs/Int8.h>
+
 #ifndef PI_
 #define PI_ 3.141592653589
 #endif
-
 
 class Record
 {
 	private:
 		void gps_callback(const driverless::Gps& gpsMsg);
+		void timerCallback(const ros::TimerEvent&);
+		
 		std::string file_path;
 		FILE *fp;
-		driverless::Gps last_point;
+		driverless::Gps last_point , current_point;
 		float sample_distance;
-		
+		ros::Subscriber gps_sub;
+		ros::Timer timer;
 	public:
 		Record();
 		~Record();	
 		void run();	
+		void recordToFile();
+		int mode;	
 };
 
 Record::Record()
 {
 	last_point.lon = 0.0;
 	last_point.lat = 0.0;
+	mode = 0;
 }
 
 Record::~Record()
@@ -35,23 +45,28 @@ void Record:: run()
 {
 	ros::NodeHandle nh;
 	ros::NodeHandle private_nh("~");
-	ros::Subscriber gps_sub;
 	
-	private_nh.param<std::string>("file_path",file_path,"/home/wendao/projects/catkin_ws/src/driverless/data/gps.txt");
 	
+	private_nh.param<std::string>("file_path",file_path,"$(rospack find driverless)data/gps.txt");
+	
+	private_nh.param<float>("sample_distance",sample_distance,0.1);
+    
 	fp = fopen(file_path.c_str(),"w");
 	
 	if(fp == NULL)
 	{
 		ROS_INFO("open record data file failed !!!!!");
-		return ;
+		ros::shutdown();
 	}
-	
-	private_nh.param<float>("sample_distance",sample_distance,0.1);
-	
+	 
 	gps_sub= nh.subscribe("/gps_data",1,&Record::gps_callback,this);
-	
-	ros::spin();
+	//timer = nh.createTimer(ros::Duration(0.05),&Record::timerCallback,this);
+}
+
+void Record::recordToFile()
+{
+	fprintf(fp,"%.8f\t%.8f\r\n",current_point.lon,current_point.lat);
+	ROS_INFO("%.8f\t%.8f\r\n",current_point.lon,current_point.lat);
 }
 
 
@@ -75,25 +90,66 @@ void Record::gps_callback(const driverless::Gps& gpsMsg)
 
 	if(x*x+y*y >= sample_distance*sample_distance)	
 	{
-		fprintf(fp,"%.8f\t%.8f\r\n",gpsMsg.lon,gpsMsg.lat);
-		ROS_INFO("%.8f\t%.8f\r\n",gpsMsg.lon,gpsMsg.lat);
-		last_point = gpsMsg;
+		current_point.lat = gpsMsg.lat;
+		current_point.lon = gpsMsg.lon;
+		last_point = current_point;
+	}
+	if(mode ==0)
+	{
+		fprintf(fp,"%.8f\t%.8f\r\n",current_point.lon,current_point.lat);
+		ROS_INFO("%.8f\t%.8f\r\n",current_point.lon,current_point.lat);
 	}
 }
-
 
 int main(int argc,char**argv)
 {
 	ros::init(argc,argv,"record_data_node");
 	
+	ros::NodeHandle param_nh("~");
+	
 	Record record;
+	
 	record.run();
 	
-	//ros::Timer printMsg_timer = nh.createTimer(ros::Duration(1.0),printMsg_timer_callback);
-	//ros::Rate rate(10);
+	param_nh.param<int>("record_mode",record.mode,0);
+	
+	if(record.mode != 0)	
+	{
+		struct input_event key_event;
+		
+		char key_press_flag = 0;
+			
+		int keys_fd=open("/dev/input/event1",O_RDONLY|O_NONBLOCK);//只读&非阻塞MODE
+		 
+		if(keys_fd<=0)
+		{   
+		    ROS_INFO("open key_event file failed");
+		    ros::shutdown();
+		}   
+		ros::Rate r(20);
 
+		while(ros::ok())
+		{
+			read(keys_fd,&key_event,sizeof(struct input_event));
+			//if(key_event.type==1)
+			//	printf("key %i state %i \n",key_event.code,key_event.value);
+			if(key_event.type==1 && key_event.code==28 && key_event.value !=0 && key_press_flag ==0)
+			{
+				key_press_flag = 1;
+		
+				record.recordToFile();
+			}
+			read(keys_fd,&key_event,sizeof(struct input_event));
+			if(key_event.type==1 && key_event.code==28 && key_event.value==0) key_press_flag =0;
+	
+			ros::spinOnce();
+			r.sleep();
+		}
+	}
+	else
+		ros::spin();
+		
 
 	return 0;
 
 }
-
