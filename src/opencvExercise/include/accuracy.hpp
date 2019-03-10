019ce81747887ea42df6f48bdfb4d1ffd96201ba
CastielLiu <castiel_liu@outlook.com>
2019-03-11 02:35:56 +0800

    add pkg opencvExercise

---
+++ src/opencvExercise/include/accuracy.hpp
@@ -0,0 +1,32 @@
+#ifndef _ACCURACY_HPP_
+#define _ACCURACY_HPP_
+
+#include "config.hpp"
+#include "util.hpp"
+#include "lights_recognize.hpp"
+
+namespace traffic{
+    namespace demo{
+        int accuracyTest(const char* testfolder)
+        {   
+            LightsRecognize lights;  
+            lights.setDetectType(SR_DETECT_COLOR);
+            lights.setModel(false);                   
+            std::vector<cv::Mat>outrects;
+            std::vector<std::string> fileslist;
+            //fileslist = traffic::Utils::getFiles(testfolder,true);
+            fileslist.push_back("/home/wendao/projects/test_ws/src/opencvExercise/resourses/1.png");
+            fileslist.push_back("/home/wendao/projects/test_ws/src/opencvExercise/resourses/2.png");
+            fileslist.push_back("/home/wendao/projects/test_ws/src/opencvExercise/resourses/3.png");
+            cout << "filelist: "  << fileslist.size() << endl;
+            for(auto file : fileslist)
+            {
+                cout << "files name :" << file.c_str() << endl;
+                cv::Mat src = imread(file.c_str());
+                int result = lights.SDetect(src,outrects,false); 
+            }
+            return 0;
+        }
+    }
+}
+#endif
